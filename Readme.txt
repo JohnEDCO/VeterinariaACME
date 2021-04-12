0b917a9f344c0f85689eb95ad42776189503d09e
John EDCO <jhedwar_17@gmail.com>
2021-04-12 10:49:18 -0500

    Creacion login responsive

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+cdnjs fontawesome   ->> link into <head>
+
+poppins --> Light 300, Regular 400, Semi-bold 600, Bold 700 -> copy link import
+
+font awesome icons --> name icons
+
+
---
+++ login.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+    <link rel="stylesheet" type="text/css"  href="Css/style.css">
+    <title>Inicio de sesion</title>
+</head>
+<body>
+    <img class="wave" src="Img/waveRojo.png" alt="">
+
+    <div class="contenedor">
+        <div class="img">
+            <img src="Img/Veterinaria.svg" alt="">
+        </div>
+        <div class="contenido-login">
+            <form action="">
+                <img src="Img/logoAcme.png" alt="">
+                <h2>ACME</h2>
+                <div class="input-div email">
+                    <div class="i">
+                        <i class="fas fa-envelope"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Email</h5>
+                        <input type="text" name="user" class="input">
+                    </div>
+                </div>
+                <div class="input-div password">
+                    <div class="i">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Contraseña</h5>
+                        <input type="password" name="pass" class="input">
+                    </div>
+                </div>
+
+                <input type="submit" class="btn" value="Iniciar Sesion">
+            </form>
+        </div>
+    </div>
+</body>
+    <script src="Js/app.js"></script>
+</html>
